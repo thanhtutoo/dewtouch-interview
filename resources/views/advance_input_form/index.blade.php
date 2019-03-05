7a3eb99660fcfaee70f431a5d430146fb1843617
thanhtutoo <5531305090@lamduan.mfu.ac.th>
2019-03-05 21:58:48 +0800

    added header

--- resources/views/advance_input_form/index.blade.php
+++ resources/views/advance_input_form/index.blade.php
@@ -1,8 +1,27 @@
 @extends('app')
 @section('content')
+<!-- <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
+ -->
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="/">Dew Online Test 2</a>
+    </div>
+  </div>
+</nav>   
+<br>
+<br>
+<br>
+<br>
 <div id="_token" class="hidden" data-token="{{ csrf_token() }}"></div>
 
 <div class="container">
+<h2>Advance input field</h2>
     <div class="row">
         <div class="col-md-12">
             @if (count($errors) > 0)
